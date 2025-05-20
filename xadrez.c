8e76caa8631f7be7b48f7f36079a68c6686c2be5
zaycodev <rickson.portela1@gmail.com>
2025-05-20 18:52:49 -0300

    Atualizei para o Nível Aventureiro
    Uma nova atualização do jogo "Movimentação de peças de xadrez." 
    Adicionei uma nova peça no jogo o **cavalo** com uma estrutura nova, um loop aninhado usando "while" externamente e um "for" interno.

--- xadrez.c
+++ xadrez.c
@@ -5,13 +5,18 @@
         Começando por "while" que permite uma execução repetitiva e controle sobre execução do código.
         Também usaremos a estrutura "do-while" assegura que o pedido de entrada seja feito pelo menos uma vez
         independente do valor da variável. E por fim, usaremos a estrutura "for" é ideal quando sabemos quantas vezes o programa
-        será executado. */
+        será executado. 
+
+        Atualização - Nível Aventureiro:
+        Agora, vamos adicionar uma nova peça ao nosso tabuleiro: o **cavalo**.
+        Sabemos que o cavalo é a única peça que pode pular outras, e sua movimentação em "L" é única.
+        Para simular isso, usamos loops aninhados: um "while" externo e um "for" interno, representando os dois movimentos do cavalo. */
 
 
 int main (){
         
     printf("======================================================================\n");
-    printf("    >>>>>>>>>>> DESAFIO XADREZ - NIVEL NOVATO <<<<<<<<<<\n");
+    printf("    >>>>>> MOVIMENTAÇÃO DE PEÇAS DE XADREZ - NIVEL AVENTUREIRO <<<<<\n");
     printf("======================================================================\n\n");
 
     printf("        >>>>>>>>>> Movimentação da peça: Torre <<<<<<<<<<\n");
@@ -46,7 +51,23 @@ int main (){
     {
         printf("Movendo a rainha para a esquerda... passo %d\n", rainha +1); // Imprime a direção da rainha, começando pelo passo 1
     }
-    
+    printf("\n");
+
+    printf("       >>>>>>>>>> Movimentação da peça: Cavalo <<<<<<<<<<\n"); // Nova peça no tabuleiro
+    printf("           O cavalo acaba de se mover 3 casas\n");  // Descrição do movimento da peça
+
+    int movimentoEmL = 1; // Criei uma variável descritiva que informa o último passo da nossa peça, uma flag para controlar o movimento em 'L'
+
+        while (movimentoEmL--)  // Aqui usei 'while' com decremento
+        {
+            for (int cavalo = 0; cavalo < 2; cavalo++) // O cavalo começará com 0 passos, e enquanto for menor que 2 ele vai repetir, e incrementar +1 movimento
+            {
+                printf("Movendo o cavalo para baixo... passo %d\n", cavalo +1);  // Imrprime a direção do cavalo, começando pelo passo 1 (fica mais legível assim)
+            }
+            printf("Movendo o cavalo para esquerda... passo %d\n", movimentoEmL +1); // Imprime o último movimento do cavalo, deixei começando em 1 pq é um novo movimento.
+            
+        }
+
 
 
     return 0;
